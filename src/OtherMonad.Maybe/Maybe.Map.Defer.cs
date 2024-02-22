fd381f9be6739917e397de7ebd755cdc5bc9b4f2
Erik Pique <epique@plainconcepts.com>
2024-02-22 07:44:20 +0100

    wip

---
+++ src/OtherMonad.Maybe/Maybe.Map.Defer.cs
@@ -0,0 +1,43 @@
+﻿namespace OtherMonad;
+
+public static partial class Maybe
+{
+    public static Deferred<IEnumerable<Maybe<TResult>>> Map<TSource, TResult>(this IEnumerable<Deferred<Maybe<TSource>>> sources, Func<TSource, TResult> selector)
+    {
+        ArgumentNullException.ThrowIfNull(selector, nameof(selector));
+
+        return () =>
+        {
+            var res = new List<Maybe<TResult>>();
+
+            foreach (var source in sources)
+            {
+                var deferred = source.BindDefer(src => selector(src));
+
+                res.Add(deferred());
+            }
+
+            return res;
+        };
+    }
+
+    public static DeferredTask<IEnumerable<Maybe<TResult>>> Map<TSource, TResult>(this IEnumerable<DeferredTask<Maybe<TSource>>> sources, Func<TSource, CancellationToken, Task<TResult>> selector, CancellationToken cancellation = default)
+    {
+        ArgumentNullException.ThrowIfNull(selector, nameof(selector));
+
+        return async () =>
+        {
+            var res = new List<Maybe<TResult>>();
+
+            foreach (var source in sources)
+            {
+                var deferred = source.BindDefer((src, ct) => selector(src, ct), cancellation);
+                var item = await deferred();
+
+                res.Add(item);
+            }
+
+            return res;
+        };
+    }
+}
